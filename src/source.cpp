cde089b8e3e1b0f39689389db9e12fc57c1506bf
aodq <dtoadq@gmail.com>
2020-04-03 23:29:37 -0400

    Fixes progress bar indicator

--- src/source.cpp
+++ src/source.cpp
@@ -51,9 +51,9 @@ public:
 void PrintProgress(float progress) {
   printf("[");
   for (int i = 0; i < 40; ++ i) {
-    if (i <  40.0f*progress) printf("=");
-    if (i == 40.0f*progress) printf(">");
-    if (i >  40.0f*progress) printf(" ");
+    if (i <  static_cast<int>(40.0f*progress)) printf("=");
+    if (i == static_cast<int>(40.0f*progress)) printf(">");
+    if (i >  static_cast<int>(40.0f*progress)) printf(" ");
   }
   // leading spaces in case of terminal/text corruption
   printf("] %0.1f%%   \r", progress*100.0f);
