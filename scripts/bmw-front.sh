44d3f29c9021c59ac3105e239667c554ef33935c
aodq <dtoadq@gmail.com>
2020-04-19 01:10:53 -0400

    Updates testing model scripts

--- scripts/bmw-front.sh
+++ scripts/bmw-front.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/sh
-./bin/cpuraytracer --spp 128 -O -500.0f,200.0f,200.0f -T 0.0f,0.0f,0.0f -F 90.0f -f ./test-models/mcguire/bmw/bmw.obj $@
+./bin/cpuraytracer -O -500.0f,200.0f,200.0f -T 0.0f,0.0f,0.0f -F 90.0f -f ./test-models/mcguire/bmw/bmw.obj $@
---
+++ scripts/cornellbox-original.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/sh
+./bin/cpuraytracer -O +0.0f,1.0f,+3.0f -T 0.0f,1.0f,0.0f -F 90.0f -f ./test-models/mcguire/cornell-box/CornellBox-Original.obj $@
